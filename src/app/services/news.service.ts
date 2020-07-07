d38a368e3caa8b56a287a1f716637793cc3201c1
Momcilo Savic <savic227@gmail.com>
2020-07-07 22:03:55 +0200

    created methods in service for getting top stories and items by id

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,5 +1,8 @@
+import { Item } from './../model/item.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const URL: string = "https://hacker-news.firebaseio.com/v0/";
 
@@ -9,4 +12,19 @@ const URL: string = "https://hacker-news.firebaseio.com/v0/";
 export class NewsService {
 
   constructor(private http: HttpClient) { }
+
+  getTopStoriesIds(): Observable<Array<number>> {
+    return this.http.get<Array<number>>(URL + "topstories.json").pipe(map(data => {
+      let idList: Array<number> = [];
+      data.forEach(elem => idList.push(elem));
+      return idList;
+    }));
+  }
+
+  getItemById(id): Observable<Item> {
+    return this.http.get(URL + "item/" + id + ".json").pipe(map(data => {
+      return new Item(data);
+    }));
+  }
+
 }
