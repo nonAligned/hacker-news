bb0e44751a9a4762efb28349d2fd787dab96763f
Momcilo Savic <savic227@gmail.com>
2020-07-07 20:05:57 +0200

    generated about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { NavigationComponent } from './core/navigation/navigation.component';
+import { AboutComponent } from './core/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
