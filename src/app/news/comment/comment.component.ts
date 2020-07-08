1625c6096aa645540722c8347f961259f49e9f78
Momcilo Savic <savic227@gmail.com>
2020-07-08 18:57:00 +0200

    created methods for getting child comments in component

--- src/app/news/comment/comment.component.ts
+++ src/app/news/comment/comment.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { NewsService } from './../../services/news.service';
+import { Item } from './../../model/item.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'hn-comment',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
+  @Input() comment: Item;
+  childComments: Item[];
 
-  constructor() { }
+  constructor(private service: NewsService) { }
 
   ngOnInit(): void {
+    this.childComments = [];
+    this.getChildComments();
+  }
+
+  getChildComments() {
+    for (let i in this.comment.kids) {
+      this.service.getItemById(this.comment.kids[i]).subscribe(comment => {
+        this.childComments.push(comment);
+      });
+    }
   }
 
 }
