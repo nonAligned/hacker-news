5480901e89798a840db1f1c94f0b7ff1c2cd3841
Momcilo Savic <savic227@gmail.com>
2020-07-08 17:58:04 +0200

    injected services and methods for getting comments in component

--- src/app/news/comments/comments.component.ts
+++ src/app/news/comments/comments.component.ts
@@ -1,4 +1,7 @@
+import { NewsService } from './../../services/news.service';
+import { Item } from './../../model/item.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'hn-comments',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
+  story: Item;
+  comments: Item[];
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private service: NewsService) { }
 
   ngOnInit(): void {
+    this.comments = [];
+    this.getStory();
+  }
+
+  getStory() {
+    let id;
+    this.activatedRoute.paramMap.subscribe(params => {
+      id = params.get("id");
+      this.service.getItemById(id).subscribe(story => {
+        this.story = story;
+        if (this.story.kids.length != 0) {
+          this.getComments();
+        }
+      });
+    });
+  }
+
+  getComments() {
+    for(let i in this.story.kids) {
+      this.service.getItemById(this.story.kids[i]).subscribe(comment => {
+        this.comments.push(comment);
+      });
+    }
   }
 
 }
