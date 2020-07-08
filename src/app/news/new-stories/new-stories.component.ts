1f28847215ac5f95e9b13bf1210b43be98f8f831
Momcilo Savic <savic227@gmail.com>
2020-07-08 16:58:25 +0200

    created methods for getting new stories in component

--- src/app/news/new-stories/new-stories.component.ts
+++ src/app/news/new-stories/new-stories.component.ts
@@ -1,3 +1,5 @@
+import { NewsService } from './../../services/news.service';
+import { Item } from './../../model/item.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-stories.component.scss']
 })
 export class NewStoriesComponent implements OnInit {
+  newStoriesIds: number[];
+  newStories: Item[];
 
-  constructor() { }
+  constructor(private service: NewsService) { }
 
   ngOnInit(): void {
+    this.newStories = [];
+    this.getNewStories();
+  }
+
+  getNewStories() {
+    this.service.getNewStoriesIds().subscribe(data => {
+      this.newStoriesIds = data;
+      for (let id in this.newStoriesIds) {
+        this.service.getItemById(this.newStoriesIds[id]).subscribe(data => {
+          this.newStories.push(data);
+        });
+      }
+    });
   }
 
 }
