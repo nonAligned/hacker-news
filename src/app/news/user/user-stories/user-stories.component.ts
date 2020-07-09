b2da96e0ff3028ed16b320412014276be3c74510
Momcilo Savic <savic227@gmail.com>
2020-07-09 18:41:51 +0200

    created methods and template view for user stories component

--- src/app/news/user/user-stories/user-stories.component.ts
+++ src/app/news/user/user-stories/user-stories.component.ts
@@ -1,3 +1,6 @@
+import { NewsService } from './../../../services/news.service';
+import { ActivatedRoute } from '@angular/router';
+import { Item } from './../../../model/item.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-stories.component.scss']
 })
 export class UserStoriesComponent implements OnInit {
+  userItemsIds: number[];
+  userStories: Item[];
+  options = {
+    itemsPerPage: 25,
+    currentPage: 1
+  }
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private service: NewsService) { }
 
   ngOnInit(): void {
+    this.userStories = [];
+    this.activatedRoute.parent.paramMap.subscribe(params => {
+      let userId = params.get("id");
+      this.service.getUser(userId).subscribe(user => {
+        this.userItemsIds = user.submitted;
+        for (let i=0 ; i<50 ; i++) {
+          if(i <= this.userItemsIds.length - 1) {
+            this.service.getItemById(this.userItemsIds[i]).subscribe(item => {
+              if (item.type == "story" && item.deleted == null) {
+                item.time *= 1000;
+                this.userStories.push(item);
+              }
+            })
+          }
+        }
+      });
+    });
   }
 
 }
