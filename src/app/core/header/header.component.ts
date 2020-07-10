0daf0b443705404b6bb34ab1ce7a2504d97efe87
Momcilo Savic <savic227@gmail.com>
2020-07-10 21:06:36 +0200

    added slide in animation for application header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,13 @@
+import { slideInAnimation } from './../../animations/slideInAnimation';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hn-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    slideInAnimation
+  ]
 })
 export class HeaderComponent implements OnInit {
 
